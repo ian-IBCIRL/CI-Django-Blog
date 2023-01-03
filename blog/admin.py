6b8dd0d4c017f9f24999b4d8143b4ba402b83a4a
ian-IBCIRL <ian@ibcirl.com>
2023-01-03 14:51:14 +0000

    updated admin for lint fixes

--- blog/admin.py
+++ blog/admin.py
@@ -12,6 +12,7 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
 
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
