1b7340a671f06e56b95f20ffa7f3eb3f8b90b7f2
ian-IBCIRL <ian@ibcirl.com>
2023-01-08 20:53:11 +0000

    updated debug flag

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -33,7 +34,7 @@ TEMPLATES_DIR = (
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
@@ -66,6 +67,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
